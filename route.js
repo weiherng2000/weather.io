19c6874ecb580be223cb606c03dfba153d97e44f
Tan wei herng <weiherng2000@gmail.com>
2023-07-06 17:26:46 +0800

    Added the routes

--- route.js
+++ route.js
@@ -27,3 +27,55 @@ const currentWeather = function()
       });
 
 }
+/*create searchedLocation function and pass in query as a parameter*/
+/*query is split into multiple substrings based on the & delimiter*/
+/*we then pass in those substrings as arguments for the update weather function*/
+/*... is used to spread the object/iterable into individual elements*/
+
+const searchedlocation = query => updateWeather(...query.split('&'));
+
+
+/*This means that when the URL "/" is accessed, 
+the function referenced by currentWeather 
+will be returned, but not executed.*/
+const routes = new Map([
+  ["/current-location", ()=>currentWeather],
+  ["/weather",()=>searchedlocation]
+
+]);
+
+const checkHash = function()
+{
+   /*In summary, window.location.hash.slice(1) 
+  returns the hash portion of the URL 
+  without the # character at the beginning.*/
+  const requestURL = window.location.hash.slice(1);
+
+  /*destructure requestURL into route and query*/
+  /* if requestURL contains ? we split the string into 2 substring*/
+  /*then we put the first string into route and 2nd string into query*/
+  /*else the first string will just be added in route and second string undefined*/
+
+  const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
+
+  /* we get the value and if value exists we invoke the function and pass in query as a parameter*/
+  routes.get(route) ? routes.get(route)(query) : error404();
+
+}
+
+/*activate checkHash when hash is changed*/
+window.addEventListener("hashchange",checkHash);
+
+/*load event is when whole page has loaded,such as stylesheets,iframes and scripts*/
+window.addEventListener("load",function()
+{
+  if(!window.location.hash)
+  {
+    window.location.hash = "#/current-location";
+  }
+  else
+  {
+     checkHash();
+  }
+
+});
