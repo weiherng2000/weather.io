3a674faddf9f1acc99fdb07a2b357f1908677b17
Tan wei herng <weiherng2000@gmail.com>
2023-07-08 00:23:31 +0800

    CurrentWeather

--- app.js
+++ app.js
@@ -108,3 +108,102 @@ searchfield.addEventListener("input",function()
 
      }
 });
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector("[data-current-location-btn]");
+const errorContent = document.querySelector("[data-error-content]");
+
+
+export const updateWeather = function(lat,lon)
+{
+    loading.style.display = "grid";
+    container.style.overflowY = "hidden";
+    container.classList.remove("fade-in");
+    errorContent.style.display = "none";
+
+    const currentWeatherSection = document.querySelector("[data-current-weather]");
+    const highlightSection = document.querySelector("[data-highlights]");
+    const hourlySection = document.querySelector("[data-hourly-forecast]");
+    const forecastSection = document.querySelector("[data-5-day-forecast]");
+  
+    currentWeatherSection.innerHTML = "";
+    highlightSection.innerHTML = "";
+    hourlySection.innerHTML = "";
+    forecastSection.innerHTML = "";
+
+    /*if current-location is already in the hash we disable the button*/
+    if (window.location.hash === "#/current-location") 
+    {
+        currentLocationBtn.setAttribute("disabled", "");
+    } else {
+        currentLocationBtn.removeAttribute("disabled");
+    }
+
+    
+    /**
+     * CURRENT WEATHER SECTION
+     */
+    fetchData(url.currentWeather(lat, lon), function (currentWeather) 
+    {
+        /*destructure the currentweather data into individual elements*/
+        const 
+        {
+            weather,
+            dt: dateUnix,
+            /*sys.sunrise: assigned to a variable named sunriseUnixUTC.*/
+            sys: { sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC },
+            main: { temp, feels_like, pressure, humidity },
+            visibility,
+            timezone
+        } = currentWeather
+        /*destrucutre weather*/
+        const [{ main, description, icon }] = weather;
+
+        const card = document.createElement("div");
+        card.classList.add("card", "card-lg", "current-weather-card");
+        card.innerHTML = `
+            <h2 class = "title-2">Currently</h2>
+
+            <div class = "wrapper">
+            <p class = "heading">${parseInt(temp)}&deg;<sup>c</sup></p>
+
+            <img class = "weather-icon" src = "assets/images/weather icons/${icon}.png" alt="${description} width = "64" height = "64">
+
+            </div>
+
+            <p>C${main}: ${description}</p>
+
+            <ul class = "meta-list">
+            <li class = "meta-item">
+                <span class="m-icon">calendar_today</span>
+
+                <p>${module.getDate(dateUnix,timezone)}</p>
+            </li>
+
+            <li class = "meta-item">
+                <span class = "m-icon">location_on</span>
+
+                <p class="title-3 data-location></p>
+            </li>
+
+            </ul>
+        
+        `;
+        fetchData(url.reverseGeo(lat,lon),function([{name,country}]){
+             card.querySelector["data-location"].innerHTML = `
+             ${name}, ${country}
+             `;
+        })
+
+        currentWeatherSection.appendChild(card);
+
+         /**
+         * TODAY'S HIGHLIGHTS
+         */
+
+
+    });
+
+}
+
--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { updateWeather} from "./app.js";
+import { updateWeather, error404} from "./app.js";
 
 const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474" // London
 
